c46bd365319cf06e8237f0292c099eb7dcf6e3cf
Srinivasan Velu <40140@hexaware.com>
2018-12-19 10:41:15 +0530

    ne commit

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
+var requestAPI = require('request');
 const app = express();
 app.use(express.json());
-const { WebhookClient } = require('dialogflow-fulfillment');
-const { Card, Suggestion } = require('dialogflow-fulfillment');
+const {
+    WebhookClient
+} = require('dialogflow-fulfillment');
+const {
+    Card,
+    Suggestion
+} = require('dialogflow-fulfillment');
 app.post('/', (req, res) => processWebhook(req, res));
 app.listen(3000, () => console.log('App listening on port 3000!'));
 var processWebhook = function (request, response) {
-    const agent = new WebhookClient({ request, response });
+    const agent = new WebhookClient({
+        request,
+        response
+    });
     console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
     console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-    
+
     function welcome(agent) {
         agent.add(`Welcome to my agent!`);
     }
@@ -18,12 +27,35 @@ var processWebhook = function (request, response) {
         agent.add(`I didn't understand`);
         agent.add(`I'm sorry, can you try again?`);
     }
-    function pnrStatus(){
-        agent.add("This is the pnr status");
+
+    function pnrStatus(agent) {
+        var pnr = request.body.queryResult.parameters.pnr;
+
+        var requestpromise = require("request");
+        agent.add(`Here is the status for your pnr no ${pnr}`);
+
+        var options = {
+            method: 'GET',
+            url: 'http://testapi.confirmtkt.com/api/pnr/status/' + pnr,
+            headers: {
+                'postman-token': '9a7f59ac-e271-3492-384e-950978bbef83',
+                'cache-control': 'no-cache'
+            }
+        };
+
+        requestpromise(options, function (error, response, body) {
+            if (error) throw new Error(error);
+
+            console.log(body);
+        });
+
+
+
+
     }
     let intentMap = new Map();
     intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('pnr status',pnrStatus)
+    intentMap.set('pnr status', pnrStatus)
     intentMap.set('Default Fallback Intent', fallback);
     // intentMap.set('your intent name here', yourFunctionHandler);
     // intentMap.set('your intent name here', googleAssistantHandler);
