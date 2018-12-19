c6e284643a5b38d198e7294083fd59e172ee14d7
srinivasan23121987 <ursfriendly.sri23@gmail.com>
2018-12-19 22:07:58 +0530

    new commit

--- index.js
+++ index.js
@@ -15,8 +15,25 @@ const {
 app.get('/', (req, res) => res.send('success'))
 app.post('/', (req, res) => processWebhook(req, res));
 app.listen(process.env.PORT || 3000, () => console.log('App listening on port 3000!'));
-function callApis() {
+function callApis(pnr) {
+    return new Promise((resolve, reject) => {
 
+        var requestpromise = require("request");
+        agent.add(`Here is the status for your pnr no ${pnr}`);
+
+        var options = {
+            method: 'GET',
+            url: 'http://testapi.confirmtkt.com/api/pnr/status/' + pnr,
+            headers: {
+                'postman-token': '9a7f59ac-e271-3492-384e-950978bbef83',
+                'cache-control': 'no-cache'
+            }
+        };
+        requestpromise(options, (error, body, response) => {
+            if (error) reject(error);
+            if (body) resolve(body);
+        });
+    });
 }
 var processWebhook = function (request, response) {
     const agent = new WebhookClient({
@@ -35,51 +52,51 @@ var processWebhook = function (request, response) {
         agent.add(`I'm sorry, can you try again?`);
     }
 
-    async function pnrStatus(agent) {
+    function pnrStatus(agent) {
         var pnr = request.body.queryResult.parameters.pnr;
-
-        var requestpromise = require("request");
-        agent.add(`Here is the status for your pnr no ${pnr}`);
-
-        var options = {
-            method: 'GET',
-            url: 'http://testapi.confirmtkt.com/api/pnr/status/' + pnr,
-            headers: {
-                'postman-token': '9a7f59ac-e271-3492-384e-950978bbef83',
-                'cache-control': 'no-cache'
-            }
-        };
-        return requestpromise(options, (error, body, response) => {
-
-            if (error) throw new Error(error);
-
+        callApis(pnr).then(function (data) {
+            let body = data;
             if (body.hasOwnProperty("Error") && body.Error) {
                 agent.add(`${body.Error}`);
                 return false;
             }
-            console.log("-----------------------Srini------------------")
-            let conv = agent.conv();
-            conv.ask(`Hello`);
-            agent.add(conv)
-            // agent.add(new Card({
-            //     title: "This is the card Title",
-            //     imageUrl: "https://www.dropbox.com/s/5t6nwhwd338p8jb/download3.png?raw=1",
-            //     text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-            // }))
-            // agent.add(new Suggestion('Quick Reply'))
-            // agent.add(new Suggestion('Suggestion'))
+            console.log("-------------Data------------------")
+            agent.add(new Card({
+                title: "This is the card Title",
+                imageUrl: "https://www.dropbox.com/s/5t6nwhwd338p8jb/download3.png?raw=1",
+                text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+            }))
+            agent.add(new Suggestion('Quick Reply'))
+            agent.add(new Suggestion('Suggestion'))
 
-        })
-        console.log("-------------Srini2-------------");
-        agent.add('srini1s')
-        
+        }).catch((error) => {
+
+        });
+
+
+        if (error) throw new Error(error);
 
     }
-    let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('pnr status', pnrStatus)
-    intentMap.set('Default Fallback Intent', fallback);
-    // intentMap.set('your intent name here', yourFunctionHandler);
-    // intentMap.set('your intent name here', googleAssistantHandler);
-    agent.handleRequest(intentMap);
+
+    // agent.add(new Card({
+    //     title: "This is the card Title",
+    //     imageUrl: "https://www.dropbox.com/s/5t6nwhwd338p8jb/download3.png?raw=1",
+    //     text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+    // }))
+    // agent.add(new Suggestion('Quick Reply'))
+    // agent.add(new Suggestion('Suggestion'))
+
+
+    console.log("-------------Srini2-------------");
+    agent.add('srini1s')
+
+
+}
+let intentMap = new Map();
+intentMap.set('Default Welcome Intent', welcome);
+intentMap.set('pnr status', pnrStatus)
+intentMap.set('Default Fallback Intent', fallback);
+// intentMap.set('your intent name here', yourFunctionHandler);
+// intentMap.set('your intent name here', googleAssistantHandler);
+agent.handleRequest(intentMap);
 }
